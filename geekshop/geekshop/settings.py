2c239b920ccb79a75d375da2b27e5a658970d6fc
Igor Kuzmin <igorkuz2018@yandex.ru>
2020-11-05 21:51:13 +0300

    static

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -89,27 +89,27 @@ TEMPLATES = [
 LOGIN_ERROR_URL = '/'
 WSGI_APPLICATION = 'geekshop.wsgi.application'
 
-
+#
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'NAME': 'geekshop',
-        'ENGINE': 'django.db.backends.postgresql',
-        'USER': 'django',
-        'PASSWORD': 'geekbrains',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'NAME': 'geekshop',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'USER': 'django',
+#         'PASSWORD': 'geekbrains',
+#         'HOST': 'localhost'
+#     }
+# }
 
 
 # Password validation
